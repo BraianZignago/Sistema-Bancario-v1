33c080e82f266e9be0bce652c11254e38a2b167b
BraianZignago <braian.zignago@estudiantes.utec.edu.uy>
2023-12-08 22:00:49 -0300

    Melhorias no codigo

--- sistema_bancario-v1.py
+++ sistema_bancario-v1.py
@@ -14,11 +14,9 @@ LIMITE_DE_SAQUES = 3
 info_conta = []
 
 def extrato(dados):
-    i = 0
     if dados == " ":
         for elementos in info_conta:
-            print(f"-{info_conta[i]}\n")
-            i += 1
+            print(f"-{elementos}\n")
         print(f"Seu saldo atual é de R${saldo}.")
         
     else:
@@ -31,16 +29,20 @@ while True:
         case "d":
             print("Voce entrou na aba de Deposito do banco!!!\n")
             valor_depositando = int(input("Digite o valor do seu deposito: "))
+
             if valor_depositando > 0:
                 print(f"Seu deposito de R${valor_depositando} foi realizado com sucesso")
                 saldo += valor_depositando
                 send_extrato = f"Foi depositado R${valor_depositando}."
                 extrato(send_extrato)
+
             else:
                 print("ERROR -Informe um valor maior de 0-")
+
         case "s":
             print("Voce entrou na aba de Saque do banco!!!\n")
             valor_do_saque = int(input("Digite o valor desejado para o saque: "))
+
             if (valor_do_saque < saldo) and (numero_de_saques < LIMITE_DE_SAQUES) and (valor_do_saque <= limite):
                 saldo -= valor_do_saque
                 numero_de_saques += 1
@@ -48,17 +50,23 @@ while True:
                 send_extrato = f"Saque de R${valor_do_saque} realizado. Seu saldo é de R${saldo}"
                 extrato(send_extrato)
                 print(f"O seu saque de R${valor_do_saque} foi realizado com sucesso")
+
             elif valor_do_saque > saldo:
                 print("ERROR!! -Saldo insuficente-")
+
             elif valor_do_saque > limite:
                 print("ERROR -Seu limite de saques em dinhero foi atinjido-")
+
             else:
                 print("ERROR -Voce ultrapassou o seus limites de saque dirarios-")
+
         case "e":
             send_extrato = " "
             extrato(send_extrato)
+
         case "q":
             print("Muito obrigado por usar nosso sistema de bancos SISCRED")
+            
             break
         case _:
             print("invalido. Inssira uma opção correta")
